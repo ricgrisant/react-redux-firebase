382ed1308692dd591d4155b5835c9a44780a88c7
Ricardo <ricardogrijalva10@gmail.com>
2020-09-18 23:32:55 -0600

    Actions

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,13 @@
 export const createProject = (project) => {
     return (dispatch,getState, {getFirebase,getFirestore}) => {
         //make async call to DB
+        const firestore = getFirestore();
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstname: 'Ricardo',
+            authorLastname: 'Grijalva'
+
+        })
         dispatch({type: 'CREATE_PROJECT', project: project});
     }
 }
