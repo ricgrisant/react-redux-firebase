45c912aa4c4fd5bc2d1f61b9d49291271a878bdb
Ricardo <ricardogrijalva10@gmail.com>
2020-11-03 23:06:04 -0600

    Create Project

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -6,7 +6,7 @@ const ProjectSummary = ({project}) => {
         <div className="card z-depth-0 project-summary">
             <div className="card-content black-text text-darken-4">
                 <span className="card-title">{project.title}</span>
-                <p>Posted By Ricardo</p>
+                <p>Posted By {project.authorFirstname} {project.authorLastname}</p>
                 <p className="grey-text">3rd October, 5am</p>
             </div>
         </div>
